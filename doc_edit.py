e66e32dab74e5f66fff239776f81cb931338ebcd
Miguel Almeida <miguel.almeida1009@gmail.com>
2023-02-17 20:58:19 -0300

    Documentation updated

--- doc_edit.py
+++ doc_edit.py
@@ -9,7 +9,7 @@ class EditDoc:
 
     def open_document(self, dir: str) -> Document:
         """
-        Open the document to be edited.
+        Open the document .docx to be edited.
 
         :param dir: document's directory
         :return .docx file
@@ -21,9 +21,56 @@ class EditDoc:
 
         return document
 
+    def upload_form(self, dir: str, *args):
+        """
+        Upload a .json form with already saved fields that
+        can be used as a parameter in update_document() function.
+
+        :param *args: values for form keys
+        :param dir: form's directory
+        :return dict
+        """
+        try:
+            with open(dir, 'r') as fp:
+                data = json.load(fp)
+
+                if len(data) >= len(args):
+                    for i, field in enumerate(data):
+                        data[field] = args[i]
+                else:
+                    return None
+
+                return data
+
+        except FileNotFoundError as error:
+            return print(f'{error}')
+
+    def save_form(self, save_dir):
+        """
+        Saves the keys used in the update_document() function in
+        a .json file to be reused as a parameter for the same function.
+
+        :param save_dir: file's save directory
+        """
+        try:
+            form = self.form
+            save = save_dir
+
+            for i in form:
+                form[i] = ''
+
+            with open(save, 'w') as fp:
+                json.dump(form, fp)
+
+        except (FileNotFoundError, PermissionError, TypeError) as error:
+            return print(f'{error}')
+
     def update_document(self, document, save_dir, form=None, **kwargs):
         """
-        Update the document fields to new chosen ones.
+        Update the values of the keys to be changed in the document .docx;
+        If you have a .json file with predefined keys, you can use it here,
+        passing it as an argument to "form", or pass the keys and values
+        trough the **kwargs parameter.
 
         :param document: .docx file
         :param save_dir: file's save directory
@@ -49,46 +96,4 @@ class EditDoc:
             self.form = fields
 
         except (FileNotFoundError, KeyError) as error:
-            return print(f'{error}')
-
-    def upload_form(self, dir: str, *args):
-        """
-        Upload a json form with already saved fields
-
-        :param *args: values for form keys
-        :param dir: form's directory
-        :return dict
-        """
-        try:
-            with open(dir, 'r') as fp:
-                data = json.load(fp)
-
-                if len(data) >= len(args):
-                    for i, field in enumerate(data):
-                        data[field] = args[i]
-                else:
-                    return None
-
-                return data
-
-        except FileNotFoundError as error:
-            return print(f'{error}')
-
-    def save_form(self, save_dir):
-        """
-        Save the used fields into a json form
-
-        :param save_dir: file's save directory
-        """
-        try:
-            form = self.form
-            save = save_dir
-
-            for i in form:
-                form[i] = ''
-
-            with open(save, 'w') as fp:
-                json.dump(form, fp)
-
-        except (FileNotFoundError, PermissionError, TypeError) as error:
             return print(f'{error}')
